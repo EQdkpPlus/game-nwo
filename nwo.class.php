0f3405acb0dba37d9d1d78491333f5692dfb9625
Simon Wallmann <s.wallmann@kompsoft.de>
2015-01-08 20:34:50 +0100

    Bump version to 1.0.0

--- nwo.class.php
+++ nwo.class.php
@@ -26,7 +26,7 @@ if ( !defined('EQDKP_INC') ){
 if(!class_exists('nwo')) {
 	class nwo extends game_generic {
 		protected static $apiLevel	= 20;
-		public $version				= '0.2';
+		public $version				= '1.0.0';
 		protected $this_game		= 'nwo';
 		protected $types			= array('classes', 'races', 'filters');
 		protected $classes			= array();
